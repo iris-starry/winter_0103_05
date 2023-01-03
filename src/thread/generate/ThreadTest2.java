d97e00ce903885e8098618c15f2ea1ef27c7dc6a
iris-starry <iris_starry_@naver.com>
2023-01-03 14:35:18 +0900

    init commit

--- src/thread/generate/ThreadTest2.java
+++ src/thread/generate/ThreadTest2.java
@@ -4,12 +4,21 @@ public class ThreadTest2 implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < 100; i++) {
+			System.out.println("thread 실행"+(i+1));
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ThreadTest1 t2 = new ThreadTest1(); 
+		Thread t = new Thread(t2); // new born 상태
+		t.start();
 
 	}
 
